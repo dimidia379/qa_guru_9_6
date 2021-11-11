560bb6a5c761545b6b4f4c4c6cad44d0b2cd2441
dimidia379 <j.zvereva050@gmail.com>
2021-11-11 13:56:59 +0300

    initial commit

---
+++ src/test/java/guru/qa/SelenideTest.java
@@ -0,0 +1,32 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTest {
+    private static final String REPOSITORY = "selenide/selenide";
+    private static final String ISSUE_TEXT = "Bad browser language in version 6+";
+
+    @Test
+    public void githubTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com/");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(REPOSITORY);
+        $(".header-search-input").submit();
+        $(linkText(REPOSITORY)).click();
+        $(partialLinkText("Issues")).click();
+        $(withText(ISSUE_TEXT)).shouldBe(Condition.visible);
+
+
+
+    }
+}
